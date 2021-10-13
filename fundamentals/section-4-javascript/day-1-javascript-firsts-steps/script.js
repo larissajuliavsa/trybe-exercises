8d35af445779d7a88d9df19f98abeb12dbae1eba
Larissa Araujo <larissa.jvsa@gmail.com>
2021-10-13 19:22:30 -0300

    adicionei duas pastas day-1 e day-2

---
+++ fundamentals/section-4-javascript/day-1-javascript-firsts-steps/script.js
@@ -0,0 +1,30 @@
+let a = 25;
+let b = 30;
+let c = 45;
+let trybe = 28;
+
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a * b);
+//console.log(a / b);
+//console.log(a % b);
+//console.log(b > a);
+
+//if (a > b && a > c) {
+//  console.log("A é o maior dos três números")
+//} else if (b > a && b > c) {
+//  console.log("B é o maior dos três números")
+//} else if (c > a && c > b) {
+//  console.log("C é o maior dos três números")
+//}
+
+//if (a > trybe) {
+//  console.log("positive")
+//} else if (a < trybe) {
+//  console.log("negativo")
+//} else {
+//  console.log("zero")
+//}
+
+
+
---
+++ fundamentals/section-4-javascript/day-2-javascript-array-loop-for/script.js
@@ -0,0 +1,20 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  soma += numbers[i]
+}
+
+media = soma / numbers.length
+
+if (media > 20) {
+  console.log("Valor maior que 20")
+} else if (media <= 20) {
+  console.log("Valor menor ou igual a 20")
+}
+
+for (let maior in numbers) {
+  
+}
+
+console.log(media)
