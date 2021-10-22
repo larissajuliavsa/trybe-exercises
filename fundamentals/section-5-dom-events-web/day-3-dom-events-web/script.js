bc82942bd279983d9281d57e86faf34969eb9106
Larissa Araujo <larissa.jvsa@gmail.com>
2021-10-21 22:53:08 -0300

    fazendo exercises/5.3

--- fundamentals/section-5-dom-events-web/day-3-dom-events-web/script.js
+++ fundamentals/section-5-dom-events-web/day-3-dom-events-web/script.js
@@ -50,6 +50,52 @@ function setHolidaysButton(holidays) {
 setHolidaysButton('Feriados');
 
 function holidayClick() {
-  let divButton = document.querySelector('#btn-holiday');
-  let l
+  let divHoliday = document.querySelector('#btn-holiday')
+  let listHoliday = document.querySelectorAll('.holiday')  
+  let mainColor = 'rgb(238,238,238)';
+  let holidayColor = 'rgb(150, 187, 124)';
+  
+  divHoliday.addEventListener('click', function() {
+    for (let index = 0; index < listHoliday.length; index += 1) {
+      if (listHoliday[index].style.backgroundColor === holidayColor) {
+        listHoliday[index].style.backgroundColor = mainColor;         
+      } else {
+        listHoliday[index].style.backgroundColor = holidayColor;         
+      }
+    }
+  })  
 }
+
+holidayClick();
+
+function fridayButton(fridays) {
+  let daysMonth = document.querySelector('#days');
+  let fridayMonth = document.querySelectorAll('.day friday')
+  let divBttnFri = document.querySelector('.buttons-container')
+
+  let fridayBttn = document.createElement('button');
+  fridayBttn.id = 'btn-friday';
+  fridayBttn.innerText = fridays
+  divBttnFri.appendChild(fridayBttn);
+}
+
+fridayButton('Sexta-feira');
+
+function fridayClick() {
+  let buttonFriday = document.querySelector('#btn-friday');
+  let daysFriday = document.getElementsByClassName('day friday');  
+  let mainFriday = 'rgb(238,238,238)';
+  let newFriday = 'rgb(250, 213, 134)';
+
+  buttonFriday.addEventListener('click', function() {
+    for (let index = 0; index < daysFriday.length; index += 1) {
+      if (daysFriday[index].style.backgroundColor === newFriday) {
+        daysFriday[index].style.backgroundColor = mainFriday;        
+      } else {
+        daysFriday[index].style.backgroundColor = newFriday;        
+      }
+    }     
+  })
+}
+
+fridayClick()
